d8e64757947debd5255d57e5ff8472017d050e53
Gabriel Matošević <123762832+GabrielMatosevic@users.noreply.github.com>
2023-10-07 20:33:28 +0200

    Pre Merge Push

--- RiHack/Assets/Resources/GabrielResources/Scripts/GameManager.cs
+++ RiHack/Assets/Resources/GabrielResources/Scripts/GameManager.cs
@@ -12,6 +12,39 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_InputField heightInput;
     [SerializeField] private TMP_InputField weightInput;
     [SerializeField] private TMP_InputField ageInput;
+    [SerializeField] private TextMeshProUGUI[] statsTexts;
+
+    public void SetupStats(int height, int weight, int age, int gender)
+    {
+        // Define average values based on gender
+        int heightAverage = (gender == 1) ? 175 : 162;
+        int weightAverage = (gender == 1) ? 90 : 77;
+
+        // Calculate BMI for the character and average BMI
+        float BMIAverage = (float)weightAverage / (heightAverage * heightAverage);
+        float BMI = (float)weight / (height * height);
+
+        // Calculate and set Health
+        float Health = Mathf.Round(((BMI / BMIAverage) * 10) * 10);
+        PlayerPrefs.SetFloat("Health", Health);
+        statsTexts[0].text = Health.ToString();
+
+        // Calculate and set Strength
+        float Strength = Mathf.Round(age * (1f / gender * 2) * 10);
+        PlayerPrefs.SetFloat("Strength", Strength);
+        statsTexts[1].text = Strength.ToString();
+
+        // Calculate and set Speed
+        float Speed = Mathf.Round(((1f / age) * gender * 100)*10);
+        PlayerPrefs.SetFloat("Speed", Speed);
+        statsTexts[2].text = Speed.ToString();
+
+        // Calculate and set Damage
+        float Damage = Mathf.Round(((weight / (float)weightAverage) * (age / 100f) * 100) * 10);
+        PlayerPrefs.SetFloat("Damage", Damage);
+        statsTexts[3].text = Damage.ToString();
+    }
+
 
     public void SaveHeight(GameObject continueButton)
     {
@@ -64,6 +97,11 @@ public class GameManager : MonoBehaviour
         PlayerPrefs.SetInt("Gender", gender);
     }
 
+    public void ApplyChangedSettings()
+    {
+        SetupStats(PlayerPrefs.GetInt("Height", 0), PlayerPrefs.GetInt("Weight", 0), PlayerPrefs.GetInt("Age", 0), PlayerPrefs.GetInt("Gender", 0));
+    }
+
     private void Awake()
     {
         //Check gender, 0 is none, 1 is male, 2 is female
@@ -79,6 +117,10 @@ public class GameManager : MonoBehaviour
             //Load setup
             mainPanel.SetActive(false);
             setupPanel.SetActive(true);
+        }
+        else
+        {
+            SetupStats(PlayerPrefs.GetInt("Height",0), PlayerPrefs.GetInt("Weight", 0), PlayerPrefs.GetInt("Age", 0), PlayerPrefs.GetInt("Gender", 0));
         }
     }
 }
